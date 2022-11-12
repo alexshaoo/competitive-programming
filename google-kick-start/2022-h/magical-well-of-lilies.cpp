e31edb0a7f1869f87bd03c8336dc24e9f965fa64
Alex S <qwshao@uwaterloo.ca>
2022-11-12 01:06:28 -0500

    p2
    IT SHOULD WORK NO?

--- google-kick-start/2022-h/magical-well-of-lilies.cpp
+++ google-kick-start/2022-h/magical-well-of-lilies.cpp
@@ -1,27 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int MAXL = 1e5+5;
-
 int main() {
 	int T;
 	cin >> T;
-	int cost[MAXL+1];
-	for (int i = 0; i <= MAXL; ++i) {
-		cost[i] = i;
-	}
-	for (int i = 2; i < MAXL/2; ++i) {
-		int j = i;
-		int c = 4 + cost[i];
-		while (j < MAXL-i+1) {
-			c += 2;
-			j += i;
-			cost[j] = min(cost[j], c);
-		}
-	}
 	for (int t = 1; t <= T; ++t) {
 		int L;
 		cin >> L;
+		int cost[L+1];
+		for (int i = 0; i <= L; ++i) {
+			cost[i] = i;
+		}
+		for (int i = 3; i < L/2; ++i) {
+			int j = i;
+			int c = 4 + cost[i];
+			while (j < L-i+1) {
+				c += 2;
+				j += i;
+				cost[j] = min(cost[j], c);
+			}
+			for (int k = i; k < L; ++k) {
+				cost[k+1] = min(cost[k+1], cost[k] + 1);
+			}
+		}
+		for (int i = 0; i <= L; ++i) {
+			cout << i << ' ' << cost[i] << '\n';
+		}
 		printf("Case #%d: %d\n", t, cost[L]);
 	}
 }
