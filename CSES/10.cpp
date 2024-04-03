ffc68da161950185c5c60364e5349c3c00a160ad
alexshaoo <alexshao90@gmail.com>
2024-04-03 18:43:41 -0400

    10 & 11

--- CSES/10.cpp
+++ CSES/10.cpp
@@ -7,18 +7,7 @@ const int MXN = 1e5;
 const int MOD = 1e9 + 7;
 
 void solve(ll a, ll b) {
-  ll diff = abs(a - b);
-  if (diff > min(a, b)) {
-    cout << "NO" << '\n';
-    return;
-  }
-  ll tot = a + b - 3 * diff;
-  if (tot % 6 == 0) {
-    cout << "YES" << '\n';
-    return;
-  }
-  cout << "NO" << '\n';
-  return;
+  cout << ((a + b) % 3 == 0 && 2 * a >= b && 2 * b >= a ? "YES" : "NO") << '\n';
 }
 
 int main() {
