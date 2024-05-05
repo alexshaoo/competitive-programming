67692b843fc4bbedaba5e3f29c109e477b1d40e6
alexshaoo <alexshao90@gmail.com>
2024-05-04 21:42:53 -0400

    2164

--- CSES/2164.cpp
+++ CSES/2164.cpp
@@ -18,16 +18,23 @@ void print(ForwardIter begin, ForwardIter end) {
 const int MXN = 1e5 + 5;
 const int MOD = 1e9 + 7;
 
-int q;
-
-void solve() { int ans = 0; }
+int solve(int n, int k) {
+  if (n <= 1) return n;
+  int num = (n & 1) ? (n + 1) / 2 : n / 2;
+  int shift = (n & 1) ? 1 : -1;
+  if (k <= num) {
+    return (2 * k - 1) % n + 1;
+  }
+  return 2 * solve(n >> 1, k - num) + shift;
+}
 
 int main() {
   ios_base::sync_with_stdio(0);
+  int q;
   cin >> q;
   int n, k;
   while (q--) {
     cin >> n >> k;
-    solve(n, k);
+    cout << solve(n, k) << '\n';
   }
 }
