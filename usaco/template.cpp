3531094bb0ca88831f148eebe03484b257c589d1
Alex Shao <alexshao90@gmail.com>
2024-10-06 13:13:40 +0200

    phantom thnooke something

--- usaco/template.cpp
+++ usaco/template.cpp
@@ -24,10 +24,9 @@ void setIO(string s) {
 const int MXN = 1e5 + 5;
 const int MOD = 1e9 + 7;
 
-int n, ans = 0;
-
 int main() {
   // setIO("");
   ios_base::sync_with_stdio(0);
   cin.tie(0);
+  int n, ans = 0;
 }
