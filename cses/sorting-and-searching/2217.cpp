db2e708de28123240995f4f1b020f7bb66eabdb2
Alex Shao <alexshao90@gmail.com>
2026-02-18 13:01:37 -0500

    collecting numbers ii

--- cses/sorting-and-searching/2217.cpp
+++ cses/sorting-and-searching/2217.cpp
@@ -10,19 +10,42 @@ int main() {
   cin.tie(0);
   int n, m;
   cin >> n >> m;
-  vector<int> x(n), pos(n);
+  vector<int> x(n), pos(n + 1);
   for (int i = 0; i < n; ++i) {
     cin >> x[i];
     pos[x[i]] = i;
   }
   int rounds = 1;
-  for (int i = 0; i < n - 1; ++i) {
+  for (int i = 1; i < n; ++i) {
     if (pos[i] > pos[i + 1])
       ++rounds;
   }
-  cout << rounds << '\n';
+  auto countInversions = [&](int i, int j) {
+    if (x[i] > x[j])
+      swap(i, j);
+    int inv = 0;
+    if (x[i] > 1 && pos[x[i] - 1] > pos[x[i]])
+      ++inv;
+    if (x[i] < n && pos[x[i] + 1] < pos[x[i]])
+      ++inv;
+    if (abs(x[i] - x[j]) != 1) {
+      if (x[j] > 1 && pos[x[j] - 1] > pos[x[j]])
+        ++inv;
+    }
+    if (x[j] < n && pos[x[j] + 1] < pos[x[j]])
+      ++inv;
+    return inv;
+  };
   while (m--) {
-    int u, v;
-    cin >> u >> v;
+    int i, j;
+    cin >> i >> j;
+    --i;
+    --j;
+    int before = countInversions(i, j);
+    swap(x[i], x[j]);
+    swap(pos[x[i]], pos[x[j]]);
+    int after = countInversions(i, j);
+    rounds += (after - before);
+    cout << rounds << '\n';
   }
 }
