d16cd8e45950633c10fdc7015fe2590c5a3d2ef0
Alex Shao <alexshao90@gmail.com>
2026-04-01 03:31:34 -0400

    sqrt regression! idk how else to do this maybe segtree?

---
+++ cses/sorting-and-searching/2163.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+typedef long long ll;
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+  int n, k;
+  cin >> n >> k;
+  int B = max(1, (int)sqrt(n));
+  int numBlocks = (n + B - 1) / B; // ceiling
+
+  vector<vector<int>> blocks(numBlocks);
+  for (int i = 0; i < n; ++i) {
+    blocks[i / B].push_back(i + 1);
+  }
+
+  ll pos = 0;
+  for (int i = 0; i < n; ++i) {
+    pos = (pos + k) % (n - i);
+    ll p = pos;
+    int bi = 0;
+    while (p >= (int)blocks[bi].size()) {
+      p -= (int)blocks[bi].size();
+      ++bi;
+    }
+    cout << blocks[bi][p] << ' ';
+    blocks[bi].erase(blocks[bi].begin() + p);
+  }
+
+  cout << '\n';
+}
